c6e80270538318662cbbed1f15288ebf4e321bc0
keeper <keeper@36d7985e-6981-4321-a6af-859debc01d54>
2014-12-12 19:51:38 +0000

    CellularAutomata

---
+++ Assets/Scripts/Ctrl/CameraRail.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRail : MonoBehaviour 
+{
+	public GameObject target;
+	public float damping = 1;
+	public bool lookAt=true;
+	public bool rotate=true;
+	public Vector3 offset;
+	
+	public float tolerance = 0.1f;
+	
+	void Start() 
+	{
+		//		offset = transform.position - target.transform.position;
+		//		offset=new Vector3(0,0,0);
+	}
+	
+	void LateUpdate() 
+	{
+		Vector3 desiredPosition = target.transform.position + offset;
+		Vector3 position;
+		if (Vector3.Distance(desiredPosition,transform.position)<tolerance)
+		{
+			position=desiredPosition;
+		}
+		else
+		{
+			position= Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
+		}
+		
+		
+		if (lookAt) transform.LookAt(position);
+		if (rotate) transform.localRotation=Quaternion.Lerp(transform.localRotation,target.transform.localRotation,Time.deltaTime * damping);
+		transform.position = position;
+		
+		
+	}
+}
---
+++ Assets/Scripts/Ctrl/CtrlPlayerDemo.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CtrlPlayerDemo : MonoBehaviour {
+
+	public int speed=1;
+	public int turnSpeed=1;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(0,0,speed*Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(0,0,-speed*Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(-speed*Time.deltaTime,0,0);
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(speed*Time.deltaTime,0,0);
+		}
+
+
+	}
+
+	void FixedUpdate () 
+	{
+		// Generate a plane that intersects the transform's position with an upwards normal.
+		Plane playerPlane = new Plane(Vector3.up, transform.position);
+		
+		// Generate a ray from the cursor position
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		
+		// Determine the point where the cursor ray intersects the plane.
+		// This will be the point that the object must look towards to be looking at the mouse.
+		// Raycasting to a Plane object only gives us a distance, so we'll have to take the distance,
+		//   then find the point along that ray that meets that distance.  This will be the point
+		//   to look at.
+		float hitdist = 0.0f;
+		// If the ray is parallel to the plane, Raycast will return false.
+		if (playerPlane.Raycast (ray, out hitdist)) 
+		{
+			// Get the point along the ray that hits the calculated distance.
+			Vector3 targetPoint = ray.GetPoint(hitdist);
+			
+			// Determine the target rotation.  This is the rotation if the transform looks at the target point.
+			Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+			
+			// Smoothly rotate towards the target point.
+			transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+		}
+	}
+}
