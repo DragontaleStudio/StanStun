630ddc1ee02fb658375b2183e4c91cb946308b0f
tashows <tashowsgr@gmail.com>
2014-12-14 04:05:22 +0000

    music manager

---
+++ Assets/Scripts/CtrlMusicManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (AudioSource))]
+public class CtrlMusicManager : MonoBehaviour {
+
+	public AudioClip intro;
+	public AudioClip mainLoop;
+	// Use this for initialization
+	void Start () 
+	{
+		playMusic();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void playMusic()
+	{
+//		musicBank.changeVolume(0.5);
+		audio.PlayOneShot(intro);
+		StartCoroutine("playLoop");	
+	}
+
+	IEnumerator playLoop() 
+	{
+		yield return new WaitForSeconds(intro.length);
+		audio.clip = mainLoop;
+		audio.loop = true;
+		audio.Play();
+	}
+}
